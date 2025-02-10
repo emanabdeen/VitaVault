667ff2e73557eefaf92f464238a096a4fbc26eaf
Casey Snoek <snoek-c@hotmail.com>
2025-02-09 22:32:04 -0500

    Got password changing almost entirely working (just need to get the messages shown consistently in toasts or on page)

--- app/src/main/java/com/example/insight/view/MainActivity.java
+++ app/src/main/java/com/example/insight/view/MainActivity.java
@@ -2,25 +2,31 @@ package com.example.insight.view;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.insight.databinding.ActivityLoginBinding;
 import com.example.insight.databinding.ActivityMainBinding;
+import com.example.insight.utility.LoginRegisterHelper;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import java.util.concurrent.CompletableFuture;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
     FirebaseAuth mAuth;
     FirebaseUser user;
-    Button btn;
+    Button btn, changePassButton;
+    EditText oldPw, newPw, confirmPw, email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +41,14 @@ public class MainActivity extends AppCompatActivity {
             finish();
             startActivity(new Intent(MainActivity.this, Login.class));
         }
+        oldPw = binding.editTextOldPw;
+        newPw = binding.editTextNewPw;
+        confirmPw = binding.editTextConfirmPw;
+        email = binding.editTextEmail;
 
         btn = binding.button;
+        changePassButton = binding.testPasswordChange;
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -47,6 +59,24 @@ public class MainActivity extends AppCompatActivity {
                     startActivity(new Intent(MainActivity.this, Login.class));
                 }else{
                     Toast.makeText(getApplicationContext(), "You aren't logged in yet!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        changePassButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth = FirebaseAuth.getInstance();
+                if (user != null) {
+                    LoginRegisterHelper lrh = new LoginRegisterHelper();
+                    try {
+                        CompletableFuture<String> changePasswordResult = lrh.changePassword(oldPw.getText().toString(), newPw.getText().toString(), confirmPw.getText().toString(), mAuth);
+                        changePasswordResult.thenAccept( result -> {
+                            Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
+                        });
+
+                    } catch (InterruptedException e) {
+                        Log.e("MainActivity", "Change password encountered an error: " + e.getMessage());
+                    }
                 }
             }
         });
