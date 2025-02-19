59eb1380bcba772db02bc6ecbdaa3387701d7335
Casey Snoek <snoek-c@hotmail.com>
2025-02-19 14:41:27 -0500

    ocr-interface: got photo taking and displaying afterwards working correctly, next to test on real phone

--- app/src/main/java/com/example/insight/view/IngredientsScan.java
+++ app/src/main/java/com/example/insight/view/IngredientsScan.java
@@ -6,22 +6,29 @@ import android.Manifest;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import com.example.insight.databinding.ActivityIngredientsScanBinding;
 
 import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class IngredientsScan extends AppCompatActivity {
     private static final String TAG = "IngredientsScan";
@@ -29,6 +36,8 @@ public class IngredientsScan extends AppCompatActivity {
 
     private ActivityIngredientsScanBinding binding;
     private Button cameraButton;
+    private ImageView cameraResultImageView;
+    private String currentPhotoPath;
 
 
     @Override
@@ -37,6 +46,7 @@ public class IngredientsScan extends AppCompatActivity {
         binding = ActivityIngredientsScanBinding.inflate(getLayoutInflater());
         this.setContentView(binding.getRoot());
 
+        cameraResultImageView = binding.imgViewCameraResult;
         cameraButton = binding.cameraButton;
         cameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,16 +56,70 @@ public class IngredientsScan extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(TAG, "onActivityResult: called.");
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            cameraResultImageView.setImageBitmap(imageBitmap);
+        }
+    }
 
     private void dispatchTakePictureIntent() {
-        //Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        try {
-            startActivity(new Intent(MediaStore.ACTION_IMAGE_CAPTURE));
-            //startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        } catch (ActivityNotFoundException e) {
-            Log.e(TAG, "dispatchTakePictureIntent: " + e.getMessage());
-            // display error state to the user
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+                Log.d(TAG, "dispatchTakePictureIntent: " + photoFile.getAbsolutePath());
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                Log.e(TAG, "dispatchTakePictureIntent: " + ex.getMessage());
+                return;
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                currentPhotoPath = photoURI.toString();
+                Log.d(TAG, "current photo path before saving: " + currentPhotoPath);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                takePictureLauncher.launch(takePictureIntent);
+            }
+            else {
+                Log.e(TAG, "dispatchTakePictureIntent: failed to generate file uri");
+            }
         }
+    }
+
+
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        if (!storageDir.exists()) {
+            if (storageDir.mkdirs()){
+                Log.d(TAG, "createImageFile: directory created successfully");
+            } else {
+                Log.d(TAG, "createImageFile: failed to create directory " + storageDir.getName());
+            }
+        }
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 
     private void checkPermissions() {
@@ -72,6 +136,24 @@ public class IngredientsScan extends AppCompatActivity {
         }
     }
 
+    private ActivityResultLauncher takePictureLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(), result -> {
+                if (result.getResultCode() == RESULT_OK) {
+                    Log.d(TAG, "Picture taken successfully and saved to: " + currentPhotoPath);
+                    if (currentPhotoPath != null) {
+                        try {
+                            cameraResultImageView.setImageURI(Uri.parse(currentPhotoPath));
+
+                        } catch (Exception e) {
+                            Log.e(TAG, "Exception when taking picture: " + e.getMessage());
+                        }
+                    }
+                    else {
+                        Log.e(TAG, "Picture taken successfully but path was returned null.");
+                    }
+                }
+            }
+    );
 
     // Register the permissions callback, which handles the user's response to the
     // system permissions dialog. Save the return value, an instance of
